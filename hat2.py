1b5b31aed39d4393035d758ac904c22c5aaefc3e
vikasisking <vikashgup9793@gmail.com>
2025-08-25 13:34:23 +0530

    Update hat2.py

--- hat2.py
+++ hat2.py
@@ -25,13 +25,17 @@ DEV_LINK = os.getenv("DEV_LINK", "https://t.me/hiden_25")
 CHANNEL_LINK = os.getenv("CHANNEL_LINK", "https://t.me/TEAM56RJ")
 Support = os.getenv("Support", "https://t.me/RISHIHEARTMAKER")
 # Required channels for force join
-REQUIRED_CHANNELS = "@TEAM56RJ"
+REQUIRED_CHANNELS = [
+    "@TEAM56RJ",
+    "@+sC5DKprbu0E0ZGNk",
+    "@BlackHatsssss",
     #"@HunterXSigma",
     #"@+UyoEvMS5XAxkNTc0",
     #"@+2P-OUmWo1hc0NmNh",
    # "@fasttech3",
    #"@PROFESSORXRIZY",
-   # "@h2icoder",
+    "@h2icoder"
+]
    # "@freeotpss"
     #"@+pG-1Ka8IixthMzNk"
 
@@ -76,7 +80,7 @@ def get_join_channel_message():
             url = f"https://t.me/{channel[1:]}"
         else:
             url = f"https://t.me/c/{channel.replace('-100', '')}"  # For private channels with chat IDs
-        keyboard.append([InlineKeyboardButton(f"Join {channel}", url=url)])
+        keyboard.append([InlineKeyboardButton(f"🚀 Join 🚀", url=url)])
        # keyboard.append([InlineKeyboardButton(f"Join {channel2}", url=url)])
     keyboard.append([InlineKeyboardButton("✅ Check Membership", callback_data="check_membership")])
     return (
